267355b660a771681a75c0fee0883b8ab0559b60
Mohan M Gowda <Mohan.M.Gowda@in.ril.com>
2016-10-03 15:14:56 +0530

    Initial commit

---
+++ src/com/antrun/AntDemo.java
@@ -0,0 +1,14 @@
+package com.antrun;
+
+import org.testng.annotations.Test;
+
+public class AntDemo {
+	
+	@Test
+	public void test1(){
+		
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/com/antrun/AntDemo2.java
@@ -0,0 +1,12 @@
+package com.antrun;
+
+import org.testng.annotations.Test;
+
+public class AntDemo2 {
+	
+	@Test
+	public void test2(){
+		System.out.println("Hello World from Test2");
+	}
+
+}
